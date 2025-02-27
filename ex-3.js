67042c7daa37ba87844f1994c0faa81d6fd66360
nichalada <nichaladasuphachayanon@gmail.com>
2025-02-27 15:28:28 +0700

    feat: add answer to ex1-6

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,12 @@
 // Start coding here
+add = (a, b) => a + b;
+add(10,5);
+
+subtract = (a, b) => a - b;
+subtract(10,5);
+
+multiply = (a, b) => a * b;
+multiply(10,5);
+
+divide = (a, b) => a / b;
+divide(10,5);
